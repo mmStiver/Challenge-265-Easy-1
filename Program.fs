19500bbe0118de4c99f4a5a36d2d502c32b6b6aa
Mike <mikestiver@hotmail.com>
2016-05-09 14:58:12 -0400

    Initial commit for challenge

---
+++ Program.fs
@@ -0,0 +1,51 @@
+﻿let rec insertions x = function
+    | []             -> [[x]]
+    | (y :: ys) as l -> (x::l)::(List.map (fun x -> y::x) (insertions x ys))
+
+let rec permutations = function
+    | []      -> seq [ [] ]
+    | x :: xs -> Seq.concat (Seq.map (insertions x) (permutations xs)) 
+
+let rec combination_r acc size set = 
+    seq { match size, set with 
+            | n, x::xs -> 
+                if n > 0 then yield! combination_r (x::acc) (n - 1) xs
+                if n >= 0 then yield! combination_r acc n xs 
+            | 0, [] -> yield acc 
+            | _, [] -> () }
+
+let combinations size set =
+    combination_r [] size set
+
+[<EntryPoint>]
+let main argv = 
+    permutations [ 0 .. 5 ]
+        |> Seq.sortBy (fun x -> (x.[0],x.[1],x.[2],x.[3]))
+        |> Seq.skip 239
+        |> Seq.take 1
+        |> Seq.toList
+        |> printfn "%A"
+
+    permutations [ 0 .. 6 ]
+        |> Seq.sortBy (fun x -> (x.[0],x.[1],x.[2],x.[3],x.[4]))
+        |> Seq.skip 3239
+        |> Seq.take 1
+        |> Seq.toList
+        |> printfn "%A"
+
+    combinations 3 [0 .. 7]
+        |> Seq.map(fun com -> List.sort com)
+        |> Seq.skip 23
+        |> Seq.take 1
+        |> Seq.toList
+        |> printfn "%A"
+
+    combinations 4 [0 .. 8]
+        |> Seq.map(fun com -> List.sort com)
+        |> Seq.skip 111
+        |> Seq.take 1
+        |> Seq.toList
+        |> printfn "%A"
+
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,21 @@
+Permutations:
+what is 240th permutation of 6
+what is 3240th permutation of 7
+
+output:
+1 5 4 3 2 0
+4 2 6 5 3 1 0
+
+combinations:
+input:
+24th combination of 3 out of 8
+112th combination of 4 out of 9
+
+output
+1 2 5
+3 4 5 6
+
+Note:
+For this challenge I copied an algorithm on Permutations and Combinations respectively. It is probably against the spirit of the Challenge, but my desire to not re-implement the same mathy algorithms I spent years in school learning is the same reason I picked Daily Challagnes over Project Euler.
+
+I had more interest in sorting the list itself. I had a much more in-depth investigation of sorting sequences of lists. I initially tried to dynamically create a sorting comparison function for the sequences, but many type exceptions later I ended up just hard coding Tuples for sorting.
